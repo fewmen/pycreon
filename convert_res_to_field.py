f9b74a86b1c02d0ae1f047c4afa8dd17fcdf704d
정진문 <fewmen2020.dev@gmail.com>
2022-04-19 02:34:22 +0900

    How can i solve this problem?

--- convert_res_to_field.py
+++ convert_res_to_field.py
@@ -1,45 +1,86 @@
+import inspect
 import os, io
 
 RES_FILE_PATH = 'C:\\eBest\\xingAPI\\Res\\'
 FILE_LIST = os.listdir(RES_FILE_PATH)
 field = io.open("fields.txt", mode="a", encoding="utf-8")
 
+for filename in FILE_LIST:
+    with open(RES_FILE_PATH+filename) as fp:
+        InBlock_begin_idx = 0
+        InBlock_end_idx = 0
+        OutBlock_begin_idx = []
+        OutBlock_end_idx = []
+        for i, line in enumerate(fp):
+            splited_word = line.replace(';', '').split(',')
+            cnt = len(splited_word)
+            if cnt >= 3 and cnt < 5:
+                for w in splited_word:
+                    if w.find("InBlock") != -1:
+                        InBlock_begin_idx = i+1
+                    elif w.find("output") != -1:
+                        OutBlock_begin_idx.append(i+1)
+                        InBlock_end_idx = i - 1
+            elif cnt == 1:
+                if splited_word[0].strip().replace('\t','') == "end" \
+                and i > InBlock_end_idx \
+                and InBlock_begin_idx < InBlock_end_idx:
+                    OutBlock_end_idx.append(i)
+                else:
+                    continue
+    fp.close
 
-# open_file = io.open(RES_FILE_PATH+FILE_LIST[0], mode="r", encoding="cp949")
-with open(RES_FILE_PATH+FILE_LIST[0]) as fp:
-    for i, line in enumerate(fp):
-        begin_idx = 0
-        end_idx = 0
-        line = line.replace(';', '')
-        splited_word = line.split(',')
-        cnt = len(splited_word)
-        if cnt == 6:
-            print('6')
-            field.write(splited_word[2].strip())
-            field.write(" = {\n")
-        elif cnt == 3:
-            print('3')
+    with open(RES_FILE_PATH+filename) as fp:
+        cidx = 0
+        for i, line in enumerate(fp):
+            splited_word = line.replace(';','').split(',')
+            cnt = len(splited_word)
+            if cnt > 4:
+                if cnt == 6 and i == 1:
+                    field.write(filename.split('.')[0])
+                    field.write(" = {\n")
+                
+                elif cnt == 5 and i == 1:
+                    field.write(filename.split('.')[0])
+                    field.write(" = {\n")
+                
+                elif cnt == 5 and i != 1:
+                    if splited_word[1].find("shcode") != -1 or splited_word[1].find("upcode") != -1 or splited_word[1].find("futcode") != -1 \
+                        or splited_word[1].find("optcode") != -1:
+                        continue
+                    field.write('        "')
+                    field.write(splited_word[1].strip().replace("\t", ""))
+                    field.write('":"')
+                    field.write(splited_word[0].strip().replace("\t", ""))
+                    field.write('"')
+                    field.write(',\n')
 
-            if "OutBlock" in splited_word:
-                print("OutBlcok in 3")
-                field.write('    "')
-                field.write(FILE_LIST[0].split('.')+splited_word[0].strip().replace("\t", ""))
-                field.write('":{\n')
-        elif cnt == 5:
-            print('5')
+            elif cnt >= 3 and cnt < 5:
+                for w in splited_word:
+                    if w.find("InBlock") != -1:
+                        continue
 
-            field.write('        "')
-            field.write(splited_word[1].strip().replace("\t", ""))
-            field.write('":"')
-            field.write(splited_word[0].strip().replace("\t", ""))
-            field.write('"')
-            field.write(',\n')
-        elif cnt == 1:
-            print('1')
+                    elif w.find("OutBlock") != -1:
+                        field.write('    "')
+                        field.write(filename.split('.')[0])
+                        field.write(w.strip().replace("\t", ""))
+                        field.write('":{\n')
+            
+            elif cnt == 1:
+                if len(OutBlock_end_idx) == 1 and OutBlock_end_idx[cidx] == i \
+                    and splited_word[0].find("end") != -1:
+                    field.write("    }\n")
+                elif len(OutBlock_end_idx) > 1 and OutBlock_end_idx[cidx] == i \
+                    and splited_word[0].find("end") != -1:
+                    if OutBlock_end_idx[-1] != i:
+                        cidx += 1
+                        field.write("    },\n")
+                elif splited_word[0].find("END_DATA_MAP") != -1:
+                        field.write("}\n\n")
+                else:
+                    continue
 
-            if "END_DATA_MAP" in splited_word:
-                print("END_DATA_MAP in 1")
-                field.write('}')
-        
+        cidx = 0
+        fp.close()
 field.close()
-fp.close()
+
--- test/test_agent_ebest.py
+++ test/test_agent_ebest.py
@@ -12,6 +12,23 @@ class TestEBest(unittest.TestCase):
     def tearDown(self):
         self.ebest.logout()
 
+    def test_get_code(self):
+        print(inspect.stack()[0][3])
+        all_result = self.ebest.get_code_list("ALL")
+        assert all_result is not None
+        kosdaq_result = self.ebest.get_code_list("KOSDAQ")
+        assert kosdaq_result is not None
+        kospi_result = self.ebest.get_code_list("KOSPI")
+        assert kospi_result is not None
+        try:
+            error_reult = self.ebest.get_code_list("KOS")
+        except:
+            error_result = None
+
+        assert error_result is None
+        print("result:", len(all_result), len(kosdaq_result), len(kospi_result))
+
+
 if __name__ == '__main__':
     test = TestEBest()
     test.setUp()
